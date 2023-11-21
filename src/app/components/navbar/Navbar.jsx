2c7eb152edd266bfbd21de61133a8504a1f30786
Hassaan Ali <hassaan ali>
2023-11-21 22:56:32 +0500

    Landing Page

---
+++ src/app/components/navbar/Navbar.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import styles from './page.module.css'
+import Image from 'next/image'
+import Link from 'next/link'
+
+const Navbar = () => {
+  return (
+    <div className='shadow-sm'>
+    <div className={`${styles.navbar} h-16 my-0 mx-auto flex items-center justify-between`}>
+        <div className='flex gap-3'>
+            <div className='flex items-center'>
+              <Image src="/mainlogo.png" alt='mainlogo' width={12} height={12} />
+            </div>
+            <Link href="/xpoarena">
+              <p className={styles.gradienttext}>XpoArena</p>
+            </Link>
+        </div>
+        <div className='flex items-center gap-10'>
+          <Link href="/xpoarena">
+            <div className={`${styles.navItemDiv} p-2`}>
+              <p className={styles.navItems}>Explore</p>
+            </div>
+          </Link>
+          <Link href="/xpoarena">
+            <div className={`${styles.navItemDiv} p-2`}>
+              <p className={styles.navItems}>Reserve</p>
+            </div>
+          </Link>
+          <Link href="/xpoarena">
+            <div className={`${styles.navItemDiv} p-2`}>
+              <p className={styles.navItems}>Manage</p>
+            </div>
+          </Link>
+        </div>
+    </div>
+    </div>
+    
+  )
+}
+
+export default Navbar
