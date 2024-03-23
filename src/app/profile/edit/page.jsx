997d25acd1653a24a67e0385c7098dd0d5382587
Hassaan Ali <hassaanali632@gmail.com>
2024-03-24 00:21:58 +0500

    Image Removed

--- src/app/profile/edit/page.jsx
+++ src/app/profile/edit/page.jsx
@@ -8,37 +8,34 @@ import styles from './page.module.css'
 export default function Profile() {
   const router = useRouter();
   const [usernames, setUsernames] = useState([]);
+  useEffect(() => {
+    async function getVerification() {
+      const res = await fetch("http://localhost:8000/api/verify/", {
+        method: 'GET',
+        credentials: 'include', // Important for cookies if using sessions
+      });
+      if (res.ok) {
+        const data = await res.json();
+        if (data.authenticated) {
+          router.push("/profile/edit");
+        } else {
+          router.push("/login")
+        }
+      }
+    }
+
+    getVerification();
+  }, [router]);
 
   const [userDetails, setUserDetails] = useState({
     first_name: '',
     last_name: '',
     email: '',
     username: '',
-    profile_picture: '',
-    profile_picture_url: '',
   });
-  const [profileImage, setProfileImage] = useState(null); // To hold the file object
-  const [imagePreview, setImagePreview] = useState('');
 
-  useEffect(() => {
 
-    if (profileImage) {
-      setImagePreview(URL.createObjectURL(profileImage));
-    } else if (userDetails.profile_picture && userDetails.profile_picture !== "") {
-      setImagePreview(`http://localhost:8000/${userDetails.profile_picture}`);
-    } else if (userDetails.profile_picture_url) {
-      setImagePreview(userDetails.profile_picture_url);
-    } else {
-      setImagePreview('/profile.png');
-    }
-  }, [profileImage, userDetails.profile_picture, userDetails.profile_picture_url]);
 
-  const handleImageChange = (e) => {
-    const file = e.target.files[0];
-    if (file) {
-      setProfileImage(file);
-    }
-  };
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -48,7 +45,7 @@ export default function Profile() {
     }));
   };
 
-  // Handler for form submission
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -64,10 +61,7 @@ export default function Profile() {
       formData.append('last_name', userDetails.last_name);
       formData.append('username', userDetails.username);
 
-      if (profileImage) {
-        formData.append('profile_picture', profileImage);
-      }
-      console.log(formData);
+      console.log(userDetails)
 
     }
 
@@ -109,19 +103,6 @@ export default function Profile() {
 
   return (
     <div className='bg-black flex items-center justify-center h-screen w-full gap-[60px]'>
-      <div className={styles.imageContainer}>
-        <label htmlFor="profileImageUpload" className="cursor-pointer">
-          <img src={imagePreview} alt="Profile" className={styles.circleImage} />
-          <input
-            type="file"
-            id="profileImageUpload"
-            name="profile_picture"
-            onChange={handleImageChange}
-            className={styles.hiddenFileInput}
-            style={{ display: 'none' }} // This hides the input but it is still functional
-          />
-        </label>
-      </div>
       <div className='flex flex-col gap-5'>
         <p className='text-white font-bold text-[30px]'>Edit Your Profile!</p>
         <div className='flex flex-col gap-4'>
