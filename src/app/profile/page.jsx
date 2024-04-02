7aed4e130aa17f7cd6468bb1ace9ebaf809ee8dd
Hassaan Ali <hassaanali632@gmail.com>
2024-04-03 04:47:17 +0500

    Profile Frontend modifications

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -288,7 +288,7 @@ export default function Profile() {
 
   }, [])
 
-  const handleLogoutClick = async() => {
+  const handleLogoutClick = async () => {
     try {
       const response = await fetch('http://localhost:8000/accounts/logout/', {
         method: 'POST',
@@ -486,7 +486,7 @@ export default function Profile() {
         }
 
         {
-          userDetails.role === "Lead" && (
+          userDetails.role === "Lead" && developerProfiles.length > 0 && (
             <div className='flex flex-col py-4 mt-5'>
               <p className='text-white font-bold text-[26px]'>Employees</p>
               <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
