91ec704efb3abeea1fc669f3315688b277f6d829
Hassaan Ali <hassaanali632@gmail.com>
2024-05-07 00:45:50 +0500

    Getting role using post service

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,7 +1,7 @@
 import apiService from "../services/apiService";
 
 const Home = () =>{
-    apiService.get("/api/home")
+    apiService.get("/api/user/148")
     return(
         <p>home page</p>
     )
--- src/app/services/apiService.js
+++ src/app/services/apiService.js
@@ -1,26 +1,29 @@
+import { getAccessToken } from "../lib/actions";
+
 const apiService = {
     get: async function (url) {
         console.log('get', url);
+        const token = await getAccessToken();
 
-        // return new Promise((resolve, reject) => {
-        //     fetch(`${process.env.NEXT_PUBLIC_API_HOST}${url}`, {
-        //         method: 'GET',
-        //         headers: {
-        //             'Accept': 'application/json',
-        //             'Content-Type': 'application/json',
-        //             'Authorization': `Bearer ${token}`
-        //         }
-        //     })
-        //     .then(response => response.json())
-        //     .then((json) => {
-        //         console.log('Response:', json);
+        return new Promise((resolve, reject) => {
+            fetch(`${process.env.NEXT_PUBLIC_API_HOST}${url}`, {
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                }
+            })
+            .then(response => response.json())
+            .then((json) => {
+                console.log('Response:', json);
 
-        //         resolve(json);
-        //     })
-        //     .catch((error) => {
-        //         reject(error);
-        //     });
-        // });
+                resolve(json);
+            })
+            .catch((error) => {
+                reject(error);
+            });
+        });
     },
 
     post: async function(url, data) {
@@ -48,29 +51,29 @@ const apiService = {
         });
     },
 
-    postWithoutToken: async function(url, data) {
-        console.log('post', url, data);
+    // postWithoutToken: async function(url, data) {
+    //     console.log('post', url, data);
 
-        return new Promise((resolve, reject) => {
-            fetch(`${process.env.NEXT_PUBLIC_API_HOST}${url}`, {
-                method: 'POST',
-                body: data,
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                }
-            })
-            .then(response => response.json())
-            .then((json) => {
-                console.log('Response:', json);
+    //     return new Promise((resolve, reject) => {
+    //         fetch(`${process.env.NEXT_PUBLIC_API_HOST}${url}`, {
+    //             method: 'POST',
+    //             body: data,
+    //             headers: {
+    //                 'Accept': 'application/json',
+    //                 'Content-Type': 'application/json'
+    //             }
+    //         })
+    //         .then(response => response.json())
+    //         .then((json) => {
+    //             console.log('Response:', json);
 
-                resolve(json);
-            })
-            .catch((error) => {
-                reject(error);
-            });
-        });
-    }
+    //             resolve(json);
+    //         })
+    //         .catch((error) => {
+    //             reject(error);
+    //         });
+    //     });
+    // }
 }
 
 export default apiService;
