744fdc6e9e36cc460d659f25c7c2fb265fd72482
Hassaan Ali <hassaanali632@gmail.com>
2024-05-08 00:35:58 +0500

    Posting event

--- src/app/components/eventmodal/EventModal.jsx
+++ src/app/components/eventmodal/EventModal.jsx
@@ -1,15 +1,60 @@
 "use client"
 
 import { useState } from "react";
+import StyledButton from "../styledbuttons/StyledButton";
+import apiService from "@/app/services/apiService";
 
 const EventModal = ({ isOpen, close }) => {
     if (!isOpen) return null;
-    const [eventName, setEventName] = useState('');
-    const [description, setDescription] = useState('');
-    const [dateOfEvent, setDateOfEvent] = useState('');
-    const [startTime, setStartTime] = useState('');
-    const [endTime, setEndTime] = useState('');
-    const [image, setImage] = useState(null);
+        const [eventName, setEventName] = useState('');
+        const [description, setDescription] = useState('');
+        const [dateOfEvent, setDateOfEvent] = useState('');
+        const [startTime, setStartTime] = useState('');
+        const [endTime, setEndTime] = useState('');
+        const [image, setImage] = useState(null);
+
+    const [errors, setErrors] = useState({
+        eventName: '',
+        description: '',
+        dateOfEvent: '',
+        startTime: '',
+        endTime: '',
+        image: ''
+    });
+
+    const scheduleEvent = async() => {
+        const newErrors = {
+            eventName: eventName ? '' : 'Event name must be filled.',
+            description: description ? '' : 'Description must be filled.',
+            dateOfEvent: dateOfEvent ? '' : 'Date of event must be filled.',
+            startTime: startTime ? '' : 'Start time must be filled.',
+            endTime: endTime ? '' : 'End time must be filled.',
+            image: image ? '' : 'Image must be uploaded.'
+        };
+
+        setErrors(newErrors);
+
+        // If any of the fields are empty, prevent form submission
+        if (Object.values(newErrors).some(error => error !== '')) return;
+
+        const formData = new FormData();
+        formData.append('eventName', eventName)
+        formData.append('description', description)
+        formData.append('dateOfEvent', dateOfEvent)
+        formData.append('startTime', startTime)
+        formData.append('endTime', endTime)
+        formData.append('image', image);
+
+        try {
+            const response = await apiService.post('/api/createevent/', formData);
+            console.log(response.data);
+        } catch (error) {
+            console.error('Error posting event:', error.response);
+        }
+
+        console.log("Event Scheduled!");
+
+    }
 
     return (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center ">
@@ -27,6 +72,8 @@ const EventModal = ({ isOpen, close }) => {
                         value={eventName}
                         onChange={(e) => setEventName(e.target.value)}
                     />
+                    {errors.eventName && <p className="text-red-500  mt-1 text-[10px]">{errors.eventName}</p>}
+
                 </div>
                 <div className="w-full">
                     <label className="text-black font-semibold font-poppins text-[12px]">
@@ -39,6 +86,8 @@ const EventModal = ({ isOpen, close }) => {
                         value={description}
                         onChange={(e) => setDescription(e.target.value)}
                     />
+                    {errors.description && <p className="text-red-500 mt-1 text-[12px]">{errors.description}</p>}
+
                 </div>
 
                 <div className="w-full">
@@ -50,6 +99,8 @@ const EventModal = ({ isOpen, close }) => {
                         className="bg-gray-50 border border-gray-300 text-gray-900 text-[12px] rounded-lg block w-full p-2.5"
                         onChange={(e) => setDateOfEvent(e.target.value)}
                     />
+                    {errors.dateOfEvent && <p className="text-red-500 mt-1 text-[12px]">{errors.dateOfEvent}</p>}
+
                 </div>
 
                 <div className="w-full flex items-center justify-center gap-3">
@@ -62,6 +113,8 @@ const EventModal = ({ isOpen, close }) => {
                             className="bg-gray-50 border border-gray-300 text-gray-900 text-[12px] rounded-lg block w-full p-2.5"
                             onChange={(e) => setStartTime(e.target.value)}
                         />
+                        {errors.startTime && <p className="text-red-500 mt-1 text-[12px]">{errors.startTime}</p>}
+
                     </div>
                     <div className="w-full">
                         <label className="text-black font-semibold font-poppins text-[12px]">
@@ -72,6 +125,8 @@ const EventModal = ({ isOpen, close }) => {
                             className="bg-gray-50 border border-gray-300 text-gray-900 text-[12px] rounded-lg block w-full p-2.5"
                             onChange={(e) => setEndTime(e.target.value)}  // Assuming you have a state variable `endTime`
                         />
+                        {errors.endTime && <p className="text-red-500 mt-1 text-[12px]">{errors.endTime}</p>}
+
                     </div>
 
                 </div>
@@ -85,10 +140,16 @@ const EventModal = ({ isOpen, close }) => {
                         accept="image/*"
                         onChange={(e) => setImage(e.target.files[0])}
                     />
+                    {errors.image && <p className="text-red-500 text-[12px]">{errors.image}</p>}
+
                 </div>
 
+                <div className="mt-3 flex flex-row gap-3 items-center justify-center">
+                    <StyledButton className="rounded-md border-2 border-red-500 text-red-500 text-[12px] font-poppins px-8 py-2" text="Close" onClick={close} />
 
-                <button onClick={close} className="mt-4 px-4 py-2 bg-red-500 text-white rounded">Close</button>
+                    <StyledButton className="rounded-md border-2 border-cgreen text-white text-[12px] font-poppins px-8 py-2 bg-cgreen" text="Schedule Event" onClick={scheduleEvent} />
+
+                </div>
             </div>
         </div>
     );
