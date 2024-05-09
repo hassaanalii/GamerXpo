f5c0a213a0f8348e94ebf9479bf6002486547eb4
Hassaan Ali <hassaanali632@gmail.com>
2024-05-10 03:24:05 +0500

    Chat integrated

--- src/app/components/inbox/ConversationDetail.jsx
+++ src/app/components/inbox/ConversationDetail.jsx
@@ -1,14 +1,80 @@
 "use client"
+import { useEffect, useState, useRef } from "react";
 import StyledButton from "../styledbuttons/StyledButton";
+import useWebSocket, {ReadyState} from "react-use-websocket";
 
-const ConversationDetail = () => {
-    const onSendClick = () => {
-        console.log("onSendClick")
+const ConversationDetail = ({token, userId, messages, conversation}) => {
+    const [newMessage, setNewMessage] = useState("")
+    const messagesDiv = useRef(null)
+    
+    const myUser = conversation.users?.find((user)=> user.id == userId)
+    const otherUser = conversation.users?.find((user)=> user.id != userId)
+    const [realtimeMessages, setRealTimeMessages] = useState([])
+
+    const { sendJsonMessage, lastJsonMessage, readyState } = useWebSocket(`ws://127.0.0.1:8000/ws/${conversation.id}/?token=${token}`, {
+        share: false,
+        shouldReconnect: () => true,
+      },
+    )
+
+    useEffect(()=>{
+        console.log('connection established', readyState)
+
+    }, [readyState])
+
+    useEffect(()=>{
+        if (lastJsonMessage && typeof lastJsonMessage === 'object' && 'name' in lastJsonMessage && 'body' in lastJsonMessage){
+            const message = {
+                id: '',
+                name: lastJsonMessage.name,
+                body: lastJsonMessage.body,
+                sent_to: otherUser,
+                created_by: myUser,
+                conversationId: conversation.id,
+            }
+
+            setRealTimeMessages((realtimeMessages) => [...realtimeMessages, message]);
+        }
+
+        scrollToBottom();
+
+    }, [lastJsonMessage])
+
+    const onSendClick = async() => {
+        console.log("heheh")
+        console.log(newMessage)
+        console.log(myUser)
+        console.log(otherUser)
+        sendJsonMessage({
+            event: 'chat_message',
+            data:{
+                body: newMessage,
+                name: myUser?.username,
+                sent_to_id: otherUser?.id,
+                conversation_id: conversation.id
+            }
+        })
+
+        setNewMessage('');
+
+        setTimeout(() => {
+            scrollToBottom()
+        }, 50);
+       
     }
+
+    const scrollToBottom = () => {
+        if (messagesDiv.current) {
+            messagesDiv.current.scrollTop = messagesDiv.current.scrollHeight;
+        }
+    }
+
+    console.log(messages)
+    console.log(realtimeMessages)
     return (
         <>
-            <div className="max-h-[400px] overflow-auto flex flex-col space-y-4 px-[100px]">
-                <div className="w-[80%] py-4 px-6 rounded-xl bg-gray-200">
+            <div ref={messagesDiv} className="max-h-[400px] overflow-auto flex flex-col space-y-4 px-[100px]">
+                {/* <div className="w-[80%] py-4 px-6 rounded-xl bg-gray-200">
                     <p className="font-bold text-gray-500 font-poppins">
                         JohnDoe
                     </p>
@@ -25,7 +91,23 @@ const ConversationDetail = () => {
                         JohnDoesdasdasdassdadsa
                     </p>
 
-                </div>
+                </div> */}
+                {messages.map((message, index)=>(
+                    <div key={index}
+                    className={`w-[80%] py-4 px-5 rounded-lg ${message.created_by.name == myUser?.name ? 'ml-[20%] bg-blue-200' : 'bg-gray-200' }`}
+                    >
+                        <p className="font-bold font-poppins text-[18px]">{message.created_by.username}</p>
+                        <p className="font-poppins text-[13px]">{message.body}</p>
+                    </div>
+                ))}
+                {realtimeMessages.map((message, index)=>(
+                    <div key={index}
+                    className={`w-[80%] py-4 px-5 rounded-lg ${message.name == myUser?.name ? 'bg-gray-200' : ' ml-[20%] bg-blue-200' }`}
+                    >
+                        <p className="font-bold font-poppins text-[18px]">{message.name}</p>
+                        <p className="font-poppins text-[13px]">{message.body}</p>
+                    </div>
+                ))}
 
             </div>
             <div className="px-[100px]">
@@ -34,6 +116,8 @@ const ConversationDetail = () => {
                         type="text"
                         placeholder="Type your message...."
                         className="w-full py-2 px-5 bg-gray-200 rounded-xl font-poppins text-[13px]"
+                        value={newMessage}
+                        onChange={(e)=> setNewMessage(e.target.value)}
                     />
                     <StyledButton text="Send" className="px-4 py-2 rounded-md bg-cgreen text-white font-poppins text-[13px] "  onClick={onSendClick}/>
 
--- src/app/home/inbox/[id]/page.jsx
+++ src/app/home/inbox/[id]/page.jsx
@@ -1,24 +1,34 @@
 import ConversationDetail from "@/app/components/inbox/ConversationDetail";
 import RoleNavBar from "@/app/components/rolenavbar/RoleNavBar";
-import { getUsername } from "@/app/lib/actions";
+import { getAccessToken, getUsername } from "@/app/lib/actions";
 import apiService from "@/app/services/apiService";
+import { redirect } from "next/navigation";
 
 const handleUsername = async () => {
     const username = await getUsername();
     return username;
 }
-const ConversationPage = async() =>{
+const ConversationPage = async({params}) =>{
+    const access = await getAccessToken();
+    if (!access) {
+        redirect("/login")
+    }
+    const response1 = await apiService.get('/api/getuser')
+
     const username = await handleUsername();
     console.log(username);
 
     const response = await apiService.get(`/api/user/${username}`);
     const role = response.role;
 
+    const conversation = await apiService.get(`/api/conversations/${params.id}`);
+
+    const token = await getAccessToken()
 
     return(
         <div className="flex flex-col gap-5">
             <RoleNavBar role={role} username={username} />
-            <ConversationDetail />
+            <ConversationDetail token={token} userId={response1.userId} messages={conversation.messages} conversation={conversation.conversation} />
     
         </div>
     )
