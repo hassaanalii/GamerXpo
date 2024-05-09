d23d91cbaa671caa37d37e65709c1fe943d1c7cb
Hassaan Ali <hassaanali632@gmail.com>
2024-05-10 00:10:51 +0500

    Test convo plus making it dynamic

--- src/app/components/inbox/Conversation.jsx
+++ src/app/components/inbox/Conversation.jsx
@@ -1,8 +1,17 @@
-const Conversation = () =>{
+'use client'
+
+import { useRouter } from "next/navigation";
+
+const Conversation = ({userId, conversation}) =>{
+    const router = useRouter()
+   
+    const otherUser = conversation.users.find((user)=> user.id != userId)
+    console.log(otherUser)
     return(
+        
         <div className="flex flex-col border-2 border-gray-300 rounded-md mt-3 gap-4 px-5 py-4">
-            <p>John Cena</p>
-            <p>Go to Conversation</p>
+            <p>{otherUser.username}</p>
+            <p onClick={()=> router.push(`/home/inbox/${conversation.id}`)}>Go to Conversation</p>
         </div>
     )
 }
--- src/app/home/inbox/page.jsx
+++ src/app/home/inbox/page.jsx
@@ -1,16 +1,29 @@
 import ChatNavBar from "@/app/components/chatnavbar/ChatNavBar";
 import Conversation from "@/app/components/inbox/Conversation";
+import { getAccessToken, getUsername } from "@/app/lib/actions";
+import apiService from "@/app/services/apiService";
+import { redirect } from "next/navigation";
 
 
-const Inbox = () => {
+const Inbox = async () => {
+    const access = await getAccessToken();
+    if (!access) {
+        redirect("/login")
+    }
+    const response = await apiService.get('/api/getuser')
+
+    const conversations = await apiService.get('/api/conversations')
     return (
         <div className="flex flex-col gap-5">
             <ChatNavBar />
             <div className="flex flex-col px-[100px]">
                 <p className="font-poppins text-[20px] text-black font-semibold">Inbox</p>
-                <Conversation />
-                <Conversation />
-                <Conversation />
+               
+                {conversations.map(conversation =>{
+                    return (
+                        <Conversation userId={response.userId} key={conversation.id} conversation={conversation} />
+                    )
+                })}
             </div>
 
         </div>
